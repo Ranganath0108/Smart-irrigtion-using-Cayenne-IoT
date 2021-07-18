9c77609a85f5e74ab00540e3df069bd2e321afb6
Ranganath <ranganathswamy.ys@gmail.com>
2021-07-18 12:53:37 +0530

    intial commit

---
+++ Project_G.ino
@@ -0,0 +1,86 @@
+
+#define CAYENNE_DEBUG
+#define CAYENNE_PRINT Serial
+#include <CayenneMQTTESP8266.h>
+#include "DHT.h"
+#define DHTTYPE DHT11
+#define DHTPIN D3
+#define valve1 D4
+//#define valve2 D5
+//#define flamepin D6
+//#define buzzer D7
+#define moisturePin A0
+
+char ssid[] = "Ranga";
+char wifiPassword[] = "9480282010";
+
+
+char username[] = "f6895170-c916-11eb-a2e4-b32ea624e442";
+char password[] = "5461705d5593d7c76fb3ca09349df28091897ab7";
+char clientID[] = "5bef1b80-c9c1-11eb-883c-638d8ce4c23d";
+
+unsigned long lastMillis = 0;
+int motor=D0;
+int Flame=HIGH;
+float moisturePercentage;
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void setup() {
+	Serial.begin(9600);
+	Cayenne.begin(username, password, clientID, ssid, wifiPassword);
+  dht.begin();
+  pinMode(motor,OUTPUT);
+  digitalWrite(motor,LOW);
+  pinMode(valve1,OUTPUT);
+  
+}
+
+void loop() {
+	Cayenne.loop();
+   float h = dht.readHumidity();
+   float t = dht.readTemperature();
+   moisturePercentage = ( 100.00 - ( (analogRead(moisturePin) / 1023.00) * 100.00 ));
+   //Flame=digitalRead(flamepin);
+   control(moisturePercentage);
+   //Firecheck(Flame);
+   
+   Cayenne.celsiusWrite(1, t);
+   Cayenne.virtualWrite(2,h,TYPE_RELATIVE_HUMIDITY,UNIT_PERCENT);
+   Cayenne.virtualWrite(3,moisturePercentage);
+  
+   
+  
+}
+CAYENNE_IN(0){
+
+digitalWrite(motor,!getValue.asInt());
+}
+
+
+void control(float p){
+  if (p<50){
+    digitalWrite(valve1,LOW);
+    }
+  else
+{digitalWrite(valve1,HIGH);}
+  
+}
+/*void Firecheck(bool F){
+if (F==LOW){
+
+  digitalWrite(buzzer,LOW);
+  digitalWrite(valve1,LOW);
+  digitalWrite(valve2,LOW);
+//  Cayenne.virtualWrite(4,1);
+
+ 
+  }
+ else{
+  digitalWrite(buzzer,HIGH);
+  digitalWrite(valve1,HIGH);
+  digitalWrite(valve2,HIGH);
+// Cayenne.virtualWrite(4,0);
+ 
+}*/
+ 
