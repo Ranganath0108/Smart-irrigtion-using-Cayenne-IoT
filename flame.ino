2856e71de9dad0939889761af29d0d12ff412af6
Ranganath <ranganathswamy.ys@gmail.com>
2021-07-18 12:55:14 +0530

    intial comits

---
+++ flame.ino
@@ -0,0 +1,26 @@
+
+#define flamepin 6
+#define out 7
+#define valve 8
+int Flame=HIGH;
+void setup() {
+  pinMode(flamepin,INPUT);
+  pinMode(out,OUTPUT);
+  pinMode(valve,OUTPUT);
+  
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Flame=digitalRead(flamepin);
+  if(Flame==LOW)
+  {digitalWrite(out,LOW);
+  digitalWrite(valve,LOW);}
+  else
+  {digitalWrite(out,HIGH);
+  digitalWrite(valve,HIGH);
+  }
+
+}
