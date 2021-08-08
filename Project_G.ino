f1a74cad0e2ed678a5edb02fe6ca737b11ff5e44
Ranganath <ranganathswamy.ys@gmail.com>
2021-08-08 16:47:00 +0530

    bug fixes and added Raindrop sensor

--- Project_G.ino
+++ Project_G.ino
@@ -2,21 +2,25 @@
 #define CAYENNE_PRINT Serial
 #include <CayenneMQTTESP8266.h>
 #include "DHT.h"
+
 #define DHTTYPE DHT11
 #define DHTPIN D3
 #define valve1 D1
 #define valve2 D2
 #define flamePin D4
 #define buzzer D5
+#define RaindropSensor D0
+#define RainLed D6 
 #define moisturePin A0
 
-char ssid[] = "Keerthi";
-char wifiPassword[] = "12345678";
+
+char ssid[] = "Ranga";
+char wifiPassword[] = "9480282010";
 
  
-char username[] = "f6895170-c916-11eb-a2e4-b32ea624e442";
-char password[] = "5461705d5593d7c76fb3ca09349df28091897ab7";
-char clientID[] = "5bef1b80-c9c1-11eb-883c-638d8ce4c23d";
+char username[] = "7a203260-f3a3-11e9-84bb-8f71124cfdfb";
+char password[] = "eadd436cf23d25093c463c77a1b6f17fc5b5b672";
+char clientID[] = "9990db40-f833-11eb-b767-3f1a8f1211ba";
 
 unsigned long lastMillis = 0;
 int motor=D8;
@@ -27,57 +31,76 @@ int FlameValue=0;
 
 
 void setup() {
-	Serial.begin(9600);
-	Cayenne.begin(username, password, clientID, ssid, wifiPassword);
+ Serial.begin(9600);
+  Cayenne.begin(username, password, clientID, ssid, wifiPassword);
   dht.begin();
   pinMode(flamePin,INPUT);
   pinMode(motor,OUTPUT);
   pinMode(valve1,OUTPUT);
   pinMode(valve2,OUTPUT);
   pinMode(buzzer,OUTPUT);
+  pinMode(RaindropSensor,INPUT);
+  pinMode(RainLed,OUTPUT);
 
-  digitalWrite(motor,LOW);
+  digitalWrite(motor,HIGH);
   digitalWrite(buzzer,HIGH);  
   
 }
 
 void loop() {
-	Cayenne.loop();
+  Cayenne.loop();
    float h = dht.readHumidity();
    float t = dht.readTemperature();
-   moisturePercentage = ( 100.00 - ( (analogRead(moisturePin) / 1023.00) * 100.00 ));
+   moisturePercentage = ( 100.00 - ( (analogRead(moisturePin) / 1023.00) * 100.00 ))+10;
+   
+   //DETECT THE RAIN
+   bool DetectRain=digitalRead(RaindropSensor);
+   if(DetectRain==HIGH){
+    digitalWrite(RainLed,HIGH);
+   }
+   else{
+    digitalWrite(RainLed,LOW);
+   }
+   
+   
    Flame=digitalRead(flamePin);
+
+   //Checking whether there is flame source or not
    if(Flame==HIGH){
     FlameValue=5;//random value for mapping high
     digitalWrite(buzzer,HIGH);
    }
    else if (Flame==LOW){
     FlameValue=8;
-    digitalWrite(buzzer,LOW);}  // random value for low
+    digitalWrite(buzzer,LOW);} 
+
+
+    //checing SoilMoisture percent
+    if(moisturePercentage>=45)
+   {digitalWrite(valve1,HIGH);
+   digitalWrite(valve2,HIGH);
+   }
+   else
+   {digitalWrite(valve1,HIGH);
+   digitalWrite(valve2,HIGH);
+}
+    
+    
+    
+    
+    // Writing in Cayenne Dashboard
    Cayenne.celsiusWrite(1, t);
    Cayenne.virtualWrite(2,h,TYPE_RELATIVE_HUMIDITY,UNIT_PERCENT);
    Cayenne.virtualWrite(3,moisturePercentage);
    Cayenne.virtualWrite(6,FlameValue);
-   
-  
-}
-CAYENNE_IN(0){
+   }
+ 
+ 
+ //getting data from iot dashboard 
 
+CAYENNE_IN(0){
 digitalWrite(motor,!getValue.asInt());
 }
 
-CAYENNE_IN(4){
 
-digitalWrite(valve1,!getValue.asInt());
-}
-
-CAYENNE_IN(5){
-
-digitalWrite(valve2,!getValue.asInt());
-}
-
-/*CAYENNE_IN(7){
-
-digitalWrite(buzzer,!getValue.asInt());
-}*/
  
